7a02831fab7480696c320a15b0cd786e36a0004d
tamimhasan19702 <tamimhasancu@gmail.com>
2022-05-23 00:13:23 +0600

    complete snake game

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ if(isCollide(snakeArr)){
   gameOverSound.play();
   musicSound.pause();
   inputDir = {x: 0, y: 0};
-  alert("Game Over. Press any key to play again!");
+  alert("khela seshhhhh!! abar khelbi??");
   snakeArr = [{x: 13, y: 15}];
   musicSound.play();
   score = 0;
